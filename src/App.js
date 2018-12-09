3ca8e5580f9094fe1405f433cfd2176429748c21
Dana Toribio <dana.toribio@noodle.ai>
2018-12-09 09:22:57 -0800

    Refactor styles into own files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { theme } from './utils'
-
-const Wrapper = styled.div``
-
-const Title = styled.h1`
-  color: ${theme.color.black};
-  font-family: ${theme.type.accent.fontFamily};
-  font-weight: ${theme.type.fontWeight.extraLight};
-  font-style: italic;x
-  font-size: 3em;
-  line-height: 1em;
-  margin-top: 1.3em;
-  text-decoration: none;
-`
+import { Wrapper } from './style'
 
 class App extends Component {
   render() {
@@ -21,7 +9,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <Wrapper>
-            <Title>Hello world! Welcome to my little space on the internet.</Title>
           </Wrapper>
         </header>
       </div>
