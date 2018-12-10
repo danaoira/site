b64efe89fc7c41e55fdea1d878c420a36e677160
Dana Toribio <dana.toribio@noodle.ai>
2018-12-09 18:48:41 -0800

    Updates

---
+++ precache-manifest.a33d8adead25b4995fd2f84229ec1de1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "048bf3ab6d92c2eae165",
+    "url": "/static/js/main.048bf3ab.chunk.js"
+  },
+  {
+    "revision": "e0f43b8bc1d06209ca3a",
+    "url": "/static/js/1.e0f43b8b.chunk.js"
+  },
+  {
+    "revision": "048bf3ab6d92c2eae165",
+    "url": "/static/css/main.9200574e.chunk.css"
+  },
+  {
+    "revision": "158473b8a2d89082b24d0791a77e4826",
+    "url": "/index.html"
+  }
+];
--- src/utils/links.js
+++
@@ -1,7 +0,0 @@
-const github = 'http://github.com/danaoira'
-const linkedin = 'http://linkedin.com/in/danaoira'
-
-export default {
-  github,
-  linkedin
-}
