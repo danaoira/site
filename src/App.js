97e91017eee9e51f78d76081a574e35235f66a8b
Dana Toribio <dana.toribio@noodle.ai>
2018-12-09 16:57:37 -0800

    WIP style guide

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
-import { Wrapper, AppWrapper } from './style'
+import { AppWrapper, Title, Subtitle, Paragraph } from './style'
 
 class App extends Component {
   render() {
     return (
       <AppWrapper>
-        <div></div>
-      </AppWrapper>
+        <Title>Style Guide</Title>
+        <section>
+          <Subtitle>Typography</Subtitle>
+          <Paragraph>
+            <div>Noto Serif TC</div>
+            <div>Raleway</div>
+          </Paragraph>
+        </section>
+
+        <section>
+          <Subtitle>Colors</Subtitle>
+          <Paragraph>
+            <div>Pink</div>
+            <div>Gray</div>
+            <div>Black</div>
+            <div>White</div>
+            <div>Yellow</div>
+          </Paragraph>
+        </section>
+        </AppWrapper>
     )
   }
 }
