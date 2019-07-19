565e90569e8c7cc428713fa7ddc3a0025b501112
Dana Toribio <dana.toribio@noodle.ai>
2019-07-19 12:30:27 -0700

    Refactor Portfolio page

---
+++ src/pages/Portfolio/settings.js
@@ -0,0 +1,44 @@
+const inProgress = [
+  {
+    title: 'World Happiness Report',
+    subtitle: 'landmark survey of global happiness'
+  }
+]
+
+const published = [
+  {
+    link: 'http://bl.ocks.org/danaoira/6b271c84d8a22789b6145ff1e82d8eb4',
+    image: 'exoplanets.png',
+    style: { 
+      backgroundPosition: '30% 75%', 
+      backgroundSize: '200%' 
+    },
+    title: 'Exoplanets',
+    subtitle: 'D3.js'
+  },
+  {
+    link: 'https://bl.ocks.org/danaoira/7e2ffa935c8da4f97681f376f34ae5a5',
+    image: 'nations.png',
+    style: {
+      backgroundPosition: '87% bottom',
+      backgroundSize: 'cover'
+    },
+    title: 'Health & Wealth of Nations',
+    subtitle: 'D3.js'
+  },
+  {
+    link: 'http://github.com/danaoira/cgv',
+    image: 'curriculum-graph-visualizer.png',
+    style: { 
+      backgroundPosition: 'left bottom',
+      backgroundSize: '200%'
+    },
+    title: 'Curriculum Graph Visualizer',
+    subtitle: 'Python, GraphViz, DOTS'
+  }
+]
+
+export {
+  inProgress,
+  published
+}
